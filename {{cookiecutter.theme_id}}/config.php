321a80d253f8df161de9e32f7b660aeb3b9bbfa6
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2018-02-14 13:59:47 +0100

    Fix comment for codechecker

--- {{cookiecutter.theme_id}}/config.php
+++ {{cookiecutter.theme_id}}/config.php
@@ -24,7 +24,7 @@
 // This line protects the file from being accessed by a URL directly.
 defined('MOODLE_INTERNAL') || die();
 
-// $THEME is defined before this page is included and we can define settings by adding properties to this global object.
+// Object $THEME is defined before this page is included and we can define settings by adding properties to this global object.
 
 // The first setting we need is the name of the theme. This should be the last part of the component name, and the same
 // as the directory name for our theme.
