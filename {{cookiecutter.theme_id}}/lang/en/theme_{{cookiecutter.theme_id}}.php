7159debadd730851ee433bd8eb12445303934f54
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2018-10-09 15:57:12 +0200

    Remove settings, we almost never want these

--- {{cookiecutter.theme_id}}/lang/en/theme_{{cookiecutter.theme_id}}.php
+++ {{cookiecutter.theme_id}}/lang/en/theme_{{cookiecutter.theme_id}}.php
@@ -24,57 +24,9 @@
 // This line protects the file from being accessed by a URL directly.
 defined('MOODLE_INTERNAL') || die();
 
-// The name of the second tab in the theme settings.
-$string['advancedsettings'] = 'Advanced settings';
-// The backgrounds tab name.
-$string['backgrounds'] = 'Backgrounds';
-// The brand colour setting.
-$string['brandcolor'] = 'Brand colour';
-// The brand colour setting description.
-$string['brandcolor_desc'] = 'The accent colour.';
 // A description shown in the admin theme selector.
-$string['choosereadme'] = 'Theme {{cookiecutter.theme_name}} is a child theme of Boost. It adds the ability to upload background photos.';
-// Name of the settings pages.
-$string['configtitle'] = '{{cookiecutter.theme_name}} settings';
-// Background image for dashboard page.
-$string['dashboardbackgroundimage'] = 'Dashboard page background image';
-// Background image for dashboard page.
-$string['dashboardbackgroundimage_desc'] = 'An image that will be stretched to fill the background of the dashboard page.';
-// Background image for default page.
-$string['defaultbackgroundimage'] = 'Default page background image';
-// Background image for default page.
-$string['defaultbackgroundimage_desc'] = 'An image that will be stretched to fill the background of all pages without a more specific background image.';
-// Background image for front page.
-$string['frontpagebackgroundimage'] = 'Front page background image';
-// Background image for front page.
-$string['frontpagebackgroundimage_desc'] = 'An image that will be stretched to fill the background of the front page.';
-// Name of the first settings tab.
-$string['generalsettings'] = 'General settings';
-// Background image for incourse page.
-$string['incoursebackgroundimage'] = 'Course page background image';
-// Background image for incourse page.
-$string['incoursebackgroundimage_desc'] = 'An image that will be stretched to fill the background of course pages.';
-// Background image for login page.
-$string['loginbackgroundimage'] = 'Login page background image';
-// Background image for login page.
-$string['loginbackgroundimage_desc'] = 'An image that will be stretched to fill the background of the login page.';
+$string['choosereadme'] = 'Theme {{cookiecutter.theme_name}} is a child theme of Boost.';
 // The name of our plugin.
 $string['pluginname'] = '{{cookiecutter.theme_name}}';
-// Preset files setting.
-$string['presetfiles'] = 'Additional theme preset files';
-// Preset files help text.
-$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href=https://docs.moodle.org/dev/Boost_Presets>Boost presets</a> for information on creating and sharing your own preset files, and see the <a href=http://moodle.net/boost>Presets repository</a> for presets that others have shared.';
-// Preset setting.
-$string['preset'] = 'Theme preset';
-// Preset help text.
-$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
-// Raw SCSS setting.
-$string['rawscss'] = 'Raw SCSS';
-// Raw SCSS setting help text.
-$string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will be injected at the end of the style sheet.';
-// Raw initial SCSS setting.
-$string['rawscsspre'] = 'Raw initial SCSS';
-// Raw initial SCSS setting help text.
-$string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
 // We need to include a lang string for each block region.
 $string['region-side-pre'] = 'Right';
